78d148e415e97d20165a05035cb02059c2c504bd
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-22 11:04:29 +0530

    File Reader

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-public class LexicalAnalyzer{
+public class LexicalAnalyzer {
 
     private static final String KEYWORD_REGEX = "\\b(?:abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\\b";
     private static final String IDENTIFIER_REGEX = "[a-zA-Z][a-zA-Z0-9]*";
@@ -20,37 +19,39 @@ public class LexicalAnalyzer{
     private static final String ARITHMETIC_OPERATOR_REGEX = "[+\\-*/%]";
     private static final String SPECIAL_SYMBOLS_REGEX = "[!@#$%^&*()_+=\\-\\[\\]{};:'\"<>,.?/\\\\|]";
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter Java File Name to be Analyzed : ");
 
         String sanitizeString = ".*\\.java$";
-        try{
+        try {
             String fileName = bufferedReader.readLine();
             Pattern pattern = Pattern.compile(sanitizeString);
             Matcher matcher = pattern.matcher(fileName);
 
-            if(matcher.matches()){
+            if (matcher.matches()) {
                 System.out.println("Java File Identified......");
                 StringBuilder inputFileAsString = new StringBuilder();
-                try{
+                try {
                     BufferedReader reader = new BufferedReader(new FileReader(fileName));
-                    
-                }catch(IOException io){
+                    String eachLine;
+
+                    while ((eachLine = reader.readLine()) != null) {
+                        inputFileAsString.append(eachLine).append("\n");
+                    }
+                    reader.close();
+                    System.out.println(inputFileAsString);
+                } catch (IOException io) {
                     System.out.println("File Not Found..");
                 }
-                
-            }else{
+
+            } else {
                 System.out.println("Enter Only Java File");
             }
-        }catch(IOException ioException){
-            
+        } catch (IOException ioException) {
+
         }
-        
 
     }
 
 }
-
-
-
